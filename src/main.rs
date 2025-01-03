691d5e1991475dcfdf59f1f3757cd63e334d071c
Кирилл Андреевич <talathel@vk.com>
2025-01-04 00:58:17 +0300

    added html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{BufRead, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = std::net::TcpListener::bind("127.0.0.1:7878")
+        .unwrap();
+    for stream in listener
+        .incoming() {
+        let stream = stream.unwrap();
+
+        handle_connections(stream);
+    }
+}
+
+fn handle_connections(mut stream: std::net::TcpStream) {
+    let buf_reader = std::io::BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = std::fs::read_to_string("resource/hello.html").unwrap();
+    let length = contents.len();
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
